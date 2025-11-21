a30d01d64fb893a1ce72cb0ca260919a6af19bac
taha mohamed mansouri <138829599+taha1545@users.noreply.github.com>
2025-11-21 20:24:30 +0100

    Optimize fetchStores call in MapPage component
    Add checks to prevent unnecessary fetch calls in useEffect.

--- components/map/MapPage.jsx
+++ components/map/MapPage.jsx
@@ -53,6 +53,9 @@ export default function MapPage() {
 
   // 
   useEffect(() => {
+     if (!userPosition) return;        // wait until location is ready
+  if (fetchedRef.current) return;   // prevent running again
+  fetchedRef.current = true;        // mark as already fetched
     const fetchStores = async () => {
       if (!userPosition) return;
       setLoadingServices(true);
